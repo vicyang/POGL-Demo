85c596b589d9481d18b97aa735f15ea0c124220b
vicyang <523066680@163.com>
2017-10-25 18:45:02 +0800

    Signed-off-by: vicyang <523066680@163.com>

---
+++ Examples/Compare/Triangle_DrawArrays.pl
@@ -0,0 +1,114 @@
+use feature 'state';
+use Time::HiRes qw/sleep time/;
+use OpenGL qw/ :all /;
+use OpenGL::Config;
+
+BEGIN
+{
+    use IO::Handle;
+    STDOUT->autoflush(1);
+    our $WinID;
+    our $HEIGHT = 500;
+    our $WIDTH  = 700;
+    our ($rx, $ry, $rz) = (0.0, 0.0, 0.0);
+
+    print "Generating triangle vertices... ";
+    our $tri_n = 20000;
+    our $vtx_n = $tri_n * 3;
+    our $ele_n = $tri_n * 3 * 3;
+    our @verts  = map { rand(1.0), rand(1.0), rand(1.0) } ( 1.. $ele_n );
+    our @colors = map { rand(1.0), rand(1.0), rand(1.0) } ( 1.. $ele_n );
+    our $verts  = OpenGL::Array->new_list( GL_FLOAT, @verts );
+    our $colors = OpenGL::Array->new_list( GL_FLOAT, @colors );
+    print "Done\n";
+}
+
+&main();
+
+sub display
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+
+    # 分量，类型，间隔，指针
+    glVertexPointer_c(3, GL_FLOAT, 0, $verts->ptr);
+    glColorPointer_c( 3, GL_FLOAT, 0, $colors->ptr);
+
+    #类型，偏移，顶点个数
+    glDrawArrays( GL_TRIANGLES, 0, $vtx_n );
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+
+    glutSwapBuffers();
+}
+
+sub idle 
+{
+    state $ta;
+    state $tb;
+
+    $ta = time();
+    sleep 0.02;
+    #glutPostRedisplay();
+    display();
+
+    $tb = time();
+    printf "%.4f\n", $tb-$ta;
+}
+
+sub init
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPointSize(5.0);
+}
+
+sub reshape
+{
+    my ($w, $h) = (shift, shift);
+    state $vthalf = 1.0;
+    state $hzhalf = 1.0;
+    state $fa = 100.0;
+
+    glViewport(0, 0, $w, $h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho( 0.0, $hzhalf, 0.0, $vthalf, 0.0, $fa*2.0); 
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0,0.0,$fa, 0.0,0.0,0.0, 0.0,1.0, $fa);
+}
+
+sub hitkey
+{
+    our $WinID;
+    my $k = lc(chr(shift));
+    if ( $k eq 'q') { quit() }
+}
+
+sub quit
+{
+    glutDestroyWindow( $WinID );
+    exit 0;
+}
+
+sub main
+{
+    our $MAIN;
+
+    glutInit();
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH_TEST | GLUT_MULTISAMPLE );
+    glutInitWindowSize($WIDTH, $HEIGHT);
+    glutInitWindowPosition(100, 100);
+    $WinID = glutCreateWindow("DrawArrays");
+    
+    &init();
+    glutDisplayFunc(\&display);
+    glutReshapeFunc(\&reshape);
+    glutKeyboardFunc(\&hitkey);
+    glutIdleFunc(\&idle);
+    glutMainLoop();
+}
---
+++ Examples/Compare/Triangle_glBegin.pl
@@ -0,0 +1,109 @@
+use feature 'state';
+use Time::HiRes qw/sleep time/;
+use OpenGL qw/ :all /;
+use OpenGL::Config;
+
+BEGIN
+{
+    use IO::Handle;
+    STDOUT->autoflush(1);
+    our $WinID;
+    our $HEIGHT = 500;
+    our $WIDTH  = 700;
+    our ($rx, $ry, $rz) = (0.0, 0.0, 0.0);
+
+    print "Generating triangle vertices... ";
+    our $tri_n = 20000;
+    our $vtx_n = $tri_n * 3;
+    our $ele_n = $tri_n * 3 * 3;
+    our @verts  = map { rand(1.0), rand(1.0), rand(1.0) } ( 1.. $ele_n );
+    our @colors = map { rand(1.0), rand(1.0), rand(1.0) } ( 1.. $ele_n );
+    print "Done\n";
+}
+
+&main();
+
+sub display
+{
+    state $i;
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    for ( $i = 0; $i < $#verts; $i+=3 )
+    {
+        glColor3f(  $colors[$i],$colors[$i+1],$colors[$i+2] );
+        glVertex3f( $verts[$i],$verts[$i+1],$verts[$i+2] );
+    }
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+sub idle 
+{
+    state $ta;
+    state $tb;
+
+    $ta = time();
+    sleep 0.02;
+    #glutPostRedisplay();
+    display();
+
+    $tb = time();
+    printf "%.4f\n", $tb-$ta;
+}
+
+sub init
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPointSize(5.0);
+}
+
+sub reshape
+{
+    my ($w, $h) = (shift, shift);
+    state $vthalf = 1.0;
+    state $hzhalf = 1.0;
+    state $fa = 100.0;
+
+    glViewport(0, 0, $w, $h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho( 0.0, $hzhalf, 0.0, $vthalf, 0.0, $fa*2.0); 
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0,0.0,$fa, 0.0,0.0,0.0, 0.0,1.0, $fa);
+}
+
+sub hitkey
+{
+    our $WinID;
+    my $k = lc(chr(shift));
+    if ( $k eq 'q') { quit() }
+}
+
+sub quit
+{
+    glutDestroyWindow( $WinID );
+    exit 0;
+}
+
+sub main
+{
+    our $MAIN;
+
+    glutInit();
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH_TEST | GLUT_MULTISAMPLE );
+    glutInitWindowSize($WIDTH, $HEIGHT);
+    glutInitWindowPosition(100, 100);
+    $WinID = glutCreateWindow("DrawArrays");
+    
+    &init();
+    glutDisplayFunc(\&display);
+    glutReshapeFunc(\&reshape);
+    glutKeyboardFunc(\&hitkey);
+    glutIdleFunc(\&idle);
+    glutMainLoop();
+}
