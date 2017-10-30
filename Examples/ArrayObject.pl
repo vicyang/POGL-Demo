8a8f27723292bd1d6b261fe21920d62a6f999292
vicyang <523066680@163.com>
2017-10-30 16:28:24 +0800

    Signed-off-by: vicyang <523066680@163.com>

--- Examples/ArrayObject.pl
+++ Examples/ArrayObject.pl
@@ -1,4 +1,5 @@
 use feature 'state';
+use feature 'say';
 use IO::Handle;
 use Time::HiRes qw/sleep/;
 use OpenGL qw/ :all /;
@@ -13,11 +14,38 @@ BEGIN
     our $WIDTH  = 700;
     my $n = 1;
 
-    my $array  = OpenGL::Array->new_list( GL_FLOAT, 1,2,3, 4,5,6 );
-    # my $o2 = OpenGL::Array->new_list(GL_FLOAT, 7, 8 ,9,  10, 11, 12);
-    #$verts->calc("1.0", "3,*", "2,+");
-    $array->calc('3,*', '2,+', 1.2 );
-    grep { printf "%.2f ", $_ ; print "\n" if $n++ % 3 == 0 } $array->retrieve();
+    my @verts = (1,2,3, 4,5,6);
+    my $array  = OpenGL::Array->new_list( GL_FLOAT, @verts );
 
+    $array->calc('3,*', '2,+', 1.2 );
+    print_array( $array );
+
+    ' 还原 ';
+    $array->calc(@verts);
+    $array->calc('2,colget','','');    ' 获取 v[2] 替换 v[0] ';
+    print_array( $array );
+
+    $array->calc('9,2,colset','','');  ' 设置 v[2] 为9，并且替换到 v[0] ';
+    print_array( $array );
+
+    ' 还原 ';
+    $array->calc(@verts);
+    $array->calc('1,2,rowget','','');     ' 获取 row[1] col[2] 元素替换到 v[0] ';
+    print_array( $array );
+
+    say 'S0 from S0, and cell[1,2] = 99 ';
+    $array->calc(@verts);
+    $array->calc('99,1,2,rowset,0,colget','',''); 
+    print_array( $array );
+
+    sub print_array
+    {
+        grep { 
+            printf "%.2f,", $_ ;
+            print "\n" if $n++ % 3 == 0 
+        }
+        ${_[0]}->retrieve();
+        print "\n";
+    }
 }
 
