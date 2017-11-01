bb1b724817265f188c40705bd63ffc7445912502
vicyang <523066680@163.com>
2017-11-01 12:07:45 +0800

    Signed-off-by: vicyang <523066680@163.com>

---
+++ Examples/Reshape_Adapt.pl
@@ -0,0 +1,95 @@
+use feature 'state';
+use IO::Handle;
+use List::Util qw/max min/;
+use Time::HiRes qw/sleep/;
+use OpenGL qw/ :all /;
+use OpenGL::Config;
+
+STDOUT->autoflush(1);
+
+BEGIN
+{
+    our $WinID;
+    our $HEIGHT = 500;
+    our $WIDTH  = 500;
+
+    our $show_w = 500;
+    our $show_h = 500;
+}
+
+&main();
+
+sub display
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glColor4f(0.3, 0.5, 0.8, 0.5);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glRectf(0.0, 0.0, 480.0, 480.0);
+
+    glColor4f(0.8, 0.5, 0.3, 0.5);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glRectf(0.0, 0.0, 498.0, 498.0);
+
+    glutSwapBuffers();
+}
+
+sub idle 
+{
+    sleep 0.02;
+    glutPostRedisplay();
+}
+
+sub init
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
+    glPointSize(5.0);
+}
+
+sub reshape
+{
+    state $fa = 100.0;
+    my ($w, $h) = (shift, shift);
+    my ($max, $min) = (max($w, $h), min($w, $h) );
+
+    glViewport(0, 0, $min, $min);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, $show_w, 0.0, $show_h, 0.0, $fa*2.0); 
+    #glFrustum(-100.0, $WIDTH-100.0, -100.0, $HEIGHT-100.0, 800.0, $fa*5.0); 
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0,0.0,$fa, 0.0,0.0,0.0, 0.0,1.0, $fa);
+}
+
+sub hitkey
+{
+    our $WinID;
+    my $k = lc(chr(shift));
+    if ( $k eq 'q') { quit() }
+}
+
+sub quit
+{
+    glutDestroyWindow( $WinID );
+    exit 0;
+}
+
+sub main
+{
+    our $MAIN;
+
+    glutInit();
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH_TEST );
+    glutInitWindowSize($WIDTH, $HEIGHT);
+    glutInitWindowPosition(100, 100);
+    $WinID = glutCreateWindow("DrawArrays");
+    
+    &init();
+    glutDisplayFunc(\&display);
+    glutReshapeFunc(\&reshape);
+    glutKeyboardFunc(\&hitkey);
+    glutIdleFunc(\&idle);
+    glutMainLoop();
+}
